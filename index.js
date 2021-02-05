48a3456e669c33f1d1a92a770541d750f8d36434
Dominic Kuncik <63456380+ItsDomHD@users.noreply.github.com>
2021-02-05 11:33:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const Client = new Discord.Client();
+Client.on('ready', () => {
+    console.log('Bot is up!')
+    fs.readdir(`./commands/`, (error, files) => {
+        if (error) return console.log(error);
+        files.forEach(file => {
+            if (!file.endsWith('.js')) return;
+            let fileProp = require(`./commands/` + file)
+            Client.api.applications(Client.user.id).guilds('758996468534214667').commands.post({
+                data: {
+                    name: fileProp.help.name,
+                    description: fileProp.help.description,
+                    options: fileProp.help.options
+                }
+            });
+            Client.ws.on('INTERACTION_CREATE', async interaction => {
+                const command = interaction.data.name.toLowerCase();
+                const args = interaction.data.options;
+                if (command == fileProp.help.name.toLowerCase()) fileProp.run(Client, interaction, args);
+            });
+        });
+    });
+});
+
+
+Client.login(require('./config.json').token);
