b56557e2d2ac956cf4d3907b4de80f1ae3fd5e1b
Dominic Kuncik <63456380+ItsDomHD@users.noreply.github.com>
2021-04-02 02:13:21 +0100

    More House Cleaning

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const Client = new Discord.Client();
-const { createConnection } = require('mysql');
 
 Client.on('ready', () => {
     console.log("====== STATUS ======")
     console.log("Bot Status: ON")
-
-    const database = createConnection({
-        host: require('./config.json').host,
-        user: require('./config.json').user,
-        password: require('./config.json').password,
-        database: require('./config.json').db
-    })
-
-    database.connect(function (err) {
-        if (err) throw err;
-        console.log("Database Status: ON")
-        console.log("====================")
-    });
+    console.log("Database Status: ON")
+    console.log("====================")
 
     fs.readdir(`./commands/`, (error, files) => {
         if (error) return console.log(error);
